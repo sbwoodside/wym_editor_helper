604cc97cb9077c908034baf8ce38f6a6b9914307
Benny Degezelle <benny@gorilla-webdesign.be>
2007-10-10 13:58:13 +0000

    updating initializer to work for IE, also using jQuery.noConflict to not clash with prototype

--- assets/javascripts/boot_wym.js
+++ assets/javascripts/boot_wym.js
@@ -1,6 +1,7 @@
-jQuery(function() {
-	jQuery('.wymeditor').wymeditor({
+var $j = jQuery.noConflict();
+$j(function() {
+	$j('.wymeditor').wymeditor({
         xhtmlParser: 'xhtml_parser.js',
-        cssParser:   'wym_css_parser.js',
+        cssParser:   'wym_css_parser.js'
     });
 });
