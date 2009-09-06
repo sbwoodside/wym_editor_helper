4c49ad4bbe9cca8e7906d3b8db68fddf6adb33c9
S Woodside <sbwoodside@yahoo.com>
2009-09-05 21:18:48 -0400

    removing unnecessary parameters and simplifying.

--- assets/javascripts/boot_wym.js
+++ assets/javascripts/boot_wym.js
@@ -1,7 +1,4 @@
-var $j = jQuery.noConflict();
-$j(function() {
-	$j('.wymeditor').wymeditor({
-        xhtmlParser: 'xhtml_parser.js',
-        cssParser:   'wym_css_parser.js'
-    });
+jQuery(function() {
+    jQuery(".wymeditor").wymeditor();
 });
+jQuery.noConflict();
